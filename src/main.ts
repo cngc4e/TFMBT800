294bf7ba4571e2788a9f2fbf0105ce3540f8bf97
cngc4e <cngc4e@gmail.com>
2021-11-18 15:39:07 +0800

    Add retry strategy

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,32 @@ import * as app from "./app";
 import { loadModules } from "./ModuleLoader";
 import { remote } from "connection/remote";
 
+const MAX_RETRIES = 15;
+
+async function tryConnect(retries: number = 0) {
+    app.info(retries == 0 ? "Starting..." : "Retrying...");
+
+    var delay = Math.min(retries * 100);
+    await new Promise((r) => setTimeout(r, delay));
+    try {
+        await app.client.run();
+    } catch (err) {
+        app.error(`Client failed to connect! (${retries})`);
+        if (retries >= MAX_RETRIES) {
+            throw `Exceeded max retries, exiting...`;
+        }
+        await tryConnect(retries + 1);
+    }
+}
+
 async function start() {
     if (process.env.REDIS_URL) {
         await remote.connect(process.env.REDIS_URL);
+        app.info("Connected to Redis");
     }
 
     await loadModules();
-
-    app.info("Starting...");
-    app.client.run();
+    await tryConnect();
 }
 
 process.nextTick(start);
