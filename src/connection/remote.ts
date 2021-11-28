9c7f0ba8270ac084ab3726407cbf97d4254ec4c1
cngc4e <cngc4e@gmail.com>
2021-11-29 03:35:03 +0800

    remote: Decode buffer from message as raw binary

--- src/connection/remote.ts
+++ src/connection/remote.ts
@@ -129,7 +129,7 @@ class InfraConnection extends TypedEmitter<InfraConnectionEvents> {
 
         // subscribe to the receiver channel
         this.client.subscribe(this.redisChannel,
-            (message, _channel) => this.onMessagePacketReceived(Buffer.from(message)));
+            (message, _channel) => this.onMessagePacketReceived(Buffer.from(message, "binary")));
 
         this.connected = true;
     }
@@ -207,7 +207,7 @@ class InfraConnection extends TypedEmitter<InfraConnectionEvents> {
         await this.client.subscribe(
             InfraConnection.broadcastToRedisChannel(channel),
             async (message, _channel) => {
-                const bcst = await this.onBroadcastPacketReceived(channel, Buffer.from(message));
+                const bcst = await this.onBroadcastPacketReceived(channel, Buffer.from(message, "binary"));
                 subscriber.emit("message", bcst.content, bcst);
                 this.emit("broadcastReceived", bcst);
             }
